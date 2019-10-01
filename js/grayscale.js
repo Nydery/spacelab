0006284794865fdfe33f034c6c73bd284bee9724
Simon Rausch-Schott <simon.rauschschott@gmail.com>
2019-10-01 21:16:33 +0200

    Update grayscale.js

--- js/grayscale.js
+++ js/grayscale.js
@@ -30,8 +30,10 @@
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
+      document.getElementById('logo').src = 'img/logo-black.png';
     } else {
       $("#mainNav").removeClass("navbar-shrink");
+      document.getElementById('logo').src = 'img/logo-white.png';
     }
   };
   // Collapse now if page is not at top
